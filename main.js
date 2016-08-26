8cd686cfb04e4b4525a4236c182b232489f461d3
Travis CI <jonathan.bouzekri@gmail.com>
2016-08-26 19:36:09 +0000

    Deploy data: 29cca26b2a3ebe6f2dfd34e63d8e5705e1e6d673

---
+++ main.js
@@ -0,0 +1,223 @@
+/**
+ * Site JS File
+ * Create the search engine webworker, bind events with the search field and manage communication between the 2.
+ * A VuJS component is tasked to update the view
+ */
+
+;(function(document, Vue, Rx, config){
+
+// Base URL of the database with cover images and articles text
+var baseDatabaseUrl = config.database.replace(/\/$/g, '');
+
+/**
+ * -----------------------------------------------
+ * Vue Component and MISC functions
+ * -----------------------------------------------
+ */
+
+/**
+ * Mapping of a type to its magazine name
+ */
+var typeToMagNameMapping = {
+  glmf: 'GNU Linux Magazine France',
+  glmfhs: 'GNU Linux Magazine France Hors Séries',
+  lp: 'Linux Pratique',
+  lphs: 'Linux Pratique Hors Séries',
+  le: 'Linux Essentiel',
+  lehs: 'Linux Essentiel Hors Séries',
+  misc: 'MISC',
+  mischs: 'MISC Hors Séries',
+  os: 'Open Silicium',
+  hm: 'Hackable Magazine'
+};
+
+/**
+ * Object used in VueJS Component to bind values to the view
+ *
+ * @namespace
+ * @property {boolean}   loading           - while true display loading animation
+ * @property {string}    loadingStep       - switch loading message (download, indexing)
+ * @property {number}    loadingProgress   - update progress bar if supported by loadingStep
+ *                                           (integer : progressbar, null: spinner, undefined: nothing)
+ * @property {boolean}   atLeastOneSearch  - true if the user has started at least one search
+ * @property {Article[]} searchResult      - display search results
+ * @property {boolean}   searchInProgress  - if the webworker is computing the result of the search query
+ * @property {string}    latestSearchTerms - latest terms searched. Used to compare with webworker search result to see if
+ *                                           we need to trigger a new search query
+ * @property {boolean}   displayError      - if we need to disable search and display error message
+ */
+var appState = {
+  loading: true,
+  loadingStep: 'download',
+  loadingProgress: undefined,
+  atLeastOneSearch: false,
+  searchResult: [],
+  searchInProgress: false,
+  latestSearchTerms: null,
+  displayError: false
+};
+
+// Register vuejs filter to display a magazine name from its type
+Vue.filter('typeToMagName', function (value) {
+  if (typeToMagNameMapping.hasOwnProperty(value)) {
+    return typeToMagNameMapping[value];
+  }
+  return 'n/a';
+});
+
+// Register vuejs filter to get the cover url of an article
+Vue.filter('articleToCover', function (article) {
+  var magNumber = "000".substring(0, 3 - ("" + article.number).length) + article.number;
+  return baseDatabaseUrl + "/" + article.type + '/' + magNumber + '.jpg';
+});
+
+// VueJS component to update HTML when object appState changes
+var appComponent = new Vue({
+  el: '#app',
+  data: appState,
+  methods: {
+    onSubmit: function() {}  // Prevent standard HTML form submit. We use RxJS to watch for input changes
+  }
+});
+
+/**
+ * Console.log a message : preprend page to identify messages coming from webpage and webworker
+ *
+ * @param {string}  message - the message to log
+ * @param {*}       [value] - any type of value to log
+ * @param {string}  [color] - hexadecimal color code to change the page prefix color
+ * @param {boolean} [error] - if true use console.error
+ */
+function logInfo(message, value, color, error) {
+  color = color || '#008d4c';
+  var data = ['%cpage', 'color: ' + color, message];
+  if (value) {
+    data.push(value);
+  }
+  if (error) {
+    console.error.apply(console, data);
+  } else {
+    console.log.apply(console, data);
+  }
+};
+
+/**
+ * Function to handle any error.
+ * It logs the error and display the error message
+ *
+ * @param {Error} e - the error raised
+ */
+function handleError(e) {
+  appState.loading = false;
+  appState.displayError = true;
+  appState.searchInProgress = false;
+  appState.searchResult = [];
+  logInfo(e, null, null, true);
+}
+
+
+/**
+ * -----------------------------------------------
+ * SearchEngine Webworker
+ * -----------------------------------------------
+ */
+
+// Create SearchEngine in a WebWorker
+var searchEngineWorker = Rx.DOM.fromWorker('worker.js');
+searchEngineWorker.sendMessage = function(message) {
+  logInfo('onNext', message);
+  this.onNext(message);
+};
+var searchEngineSharedObservable = searchEngineWorker
+  .share()
+  .map(function(e) {
+    return e.data;
+  });
+
+// Listen for loading (database downloading and indexing) messages from SearchEngine WebWorker
+searchEngineSharedObservable.filter(function(data) {
+  return data.event === 'loading';
+}).subscribe(
+  function (data) {
+    logInfo('loading', data);
+    appState.loadingProgress = data.value;
+    appState.loadingStep = data.type;
+    appState.loading = data.type !== 'ready';  // Final step when SearchEngine is ready
+  },
+  handleError
+);
+
+// Listen for search result messages from SearchEngine WebWorker
+searchEngineSharedObservable.filter(function(data) {
+  return data.event === 'result';
+}).subscribe(
+  function (data) {
+    logInfo('result', data);
+    if (data.term === appState.latestSearchTerms) {
+      logInfo('result display', '"' + data.term + '"');
+      appState.searchInProgress = false;
+      appState.searchResult = data.value;
+    } else {
+      logInfo('result does not match latest search term. Trigger new one', {received: data.term, latest: appState.latestSearchTerms});
+      searchEngineWorker.sendMessage({action: 'search', value: appState.latestSearchTerms});
+    }
+  },
+  handleError
+);
+
+// Trigger SearchEngine WebWorker indexation.
+searchEngineWorker.sendMessage({action: 'start', url: baseDatabaseUrl + '/articles.json'});
+
+
+/**
+ * -----------------------------------------------
+ * Search Field event Streams
+ * -----------------------------------------------
+ */
+
+var searchField = document.querySelector('#search-input');
+var keyUpSearchStream = Rx.DOM.keyup(searchField);
+var enterKeyPressedStream = keyUpSearchStream  // Returns input field value when Enter key is pressed
+  .filter(function (e) {
+    return e.keyCode == 13
+  })
+  .pluck('target', 'value');
+var keyUpThrottledSearchStream = keyUpSearchStream  // Returns input field value debounced
+    .pluck('target', 'value')
+    .debounce(500)
+    .distinctUntilChanged();
+
+// Watch for first key pushed in search field to stick the search field at the top of the page (kind of like Google Search)
+keyUpSearchStream.take(1).subscribe(function(e) {
+  appState.atLeastOneSearch = true;
+});
+
+// Handle form submit or clicks on submit button
+var submitButton = document.querySelector('#search-submit');
+var clickSubmitStream = Rx.Observable.fromEvent(submitButton, 'click')  // Returns input field value when form submitted or submit button clicked
+  .map(function(){
+    return searchField.value;
+  });
+
+// Watch for all streams which can trigger a search
+Rx.Observable.merge([clickSubmitStream, enterKeyPressedStream, keyUpThrottledSearchStream])
+  .distinctUntilChanged()
+  .subscribe(function(searchTerms) {
+    // Trigger search
+    appState.latestSearchTerms = searchTerms;
+    if (!appState.searchInProgress && searchTerms) {  // If no search in progress, launch one
+      appState.searchInProgress = true;
+      searchEngineWorker.sendMessage({action: 'search', value: searchTerms});
+    } else if (!searchTerms) {  // If empty search terms, cancel search
+      appState.searchInProgress = false;
+      appState.searchResult = [];
+      logInfo('empty terms - stop search');
+    } else {  // Else if search already in progress, stores term to launch search when the current one ends
+      appState.searchInProgress = true;
+      logInfo('search already in progress - storing term', '"' + searchTerms + '"');
+    }
+  });
+
+})(document, Vue, Rx, edConfig);
+
+//# sourceMappingURL=main.js.map
---
+++ worker.js
@@ -0,0 +1,229 @@
+/**
+ * Search Engine WebWorker file
+ * On startup (onmessage 'start') :
+ *  - download the database
+ *  - create the lunr index
+ * When it receives a search query (onmessage 'search'), it triggers a search in lunr index and postMessage the results
+ */
+
+// Download lunr search library (kind of a light lucene in the browser)
+importScripts('./lunr.min.js');
+
+/**
+ * Console.log a message : preprend 'worker' to identify messages coming from webpage and webworker
+ *
+ * @param {string}  message - the message to log
+ * @param {*}       [value] - any type of value to log
+ * @param {string}  [color] - hexadecimal color code to change the page prefix color
+ * @param {boolean} [error] - if true use console.error
+ */
+function logInfo(message, value, color, error) {
+  color = color || '#357ca5';
+  var data = ['%cworker', 'color: ' + color, message];
+  if (value) {
+    data.push(value);
+  }
+  if (error) {
+    console.error.apply(console, data);
+  } else {
+    console.log.apply(console, data);
+  }
+};
+
+/**
+ * Wrapper around the standard 'postMessage' method in a webworker to log when we post a message
+ *
+ * @param {string} message - the message to send back to
+ */
+function logAndPostMessage(message) {
+  logInfo('postMessage', message);
+  postMessage(message);
+};
+
+/**
+ * Create a loader to dowload the database from an URL
+ * Like jQuery.getJSON
+ *
+ * @constructor
+ *
+ * @param {string} url - the url to the database
+ */
+function UrlDataLoader(url) {
+  this.url = url;
+
+  /** @function load - takes 3 callback : success, error, onprogress. Each with one argument. */
+  this.load = function(successCallback, errorCallback, progressCallbak) {
+    logInfo('downloading from database at ' + this.url);
+
+    // Create request
+    var request = new XMLHttpRequest();
+    request.open('GET', this.url, true);
+
+    // Manage onprogress
+    if (request.onprogress) {
+      request.onprogress = function(evt) {
+        if (evt.lengthComputable) {  // If computable, returns percents. The view will display a progress bar.
+          progressCallbak(Math.round((evt.loaded / evt.total) * 100));
+        } else {  // If not computable, returns null. The view will display a spinner.
+          progressCallbak(null);
+        }
+      };
+    } else {  // If XHR2 not available, fallback to spinner in the view
+      progressCallbak(null);
+    }
+
+    // Download ended (successfully or server error)
+    request.onload = function() {
+      logInfo('database downloaded');
+      if (this.status >= 200 && this.status < 400) {
+        successCallback(JSON.parse(this.response));
+      } else {
+        errorCallback('Unable to download database');
+      }
+    };
+
+    // Error on transfert (most of the time connection error)
+    request.onerror = function() {
+      errorCallback('Unable to download database');
+    };
+
+    // Start the XHR request
+    request.send();
+  };
+};
+
+/**
+ * The SearchEngine encapsulating lunr index
+ *
+ * @constructor
+ *
+ * @param {Function} workerEventEmitter - a function to send messages when an event occurred in the engine.
+ *                                        Most of the time, the WebWorker postMessage function
+ */
+function SearchEngine(workerEventEmitter) {
+  /** Provide method for each kind of events raised by the search engine */
+  this.emitter = {
+    /** @function sendDownloadEvent - called when download start and each time the download has progressed by a bit */
+    sendDownloadEvent: function(downloaded) {
+      workerEventEmitter({event: 'loading', type: 'download', value: downloaded});
+    },
+    /** @function sendIndexingEvent - called when indexing start and each time indexing has progressed by a bit */
+    sendIndexingEvent: function(indexed) {
+      workerEventEmitter({event: 'loading', type: 'indexing', value: indexed});
+    },
+    /** @function sendIndexingEvent - called when search engine is ready to received search queries */
+    sendReadyEvent: function() {
+      workerEventEmitter({event: 'loading', type: 'ready', value: null});
+    },
+    /** @function sendIndexingEvent - called when search engine has finished to compute a search query. send results */
+    sendResultEvent: function(articles, term) {
+      workerEventEmitter({event: 'result', value: articles, term: term});
+    },
+    /** @function sendIndexingEvent - called when an error occured. kills the webworker. */
+    sendErrorEvent: function(message) {
+      workerEventEmitter({event: 'error', value: message});
+      throw Error(message);
+    }
+  };
+
+  // While loaded is false, no search query can be processed. Swithes to true when the search engine is ready.
+  this.loaded = false;
+
+  // The lunr index
+  this._index = lunr(function() {
+    this.field('title', {boost: 10});
+    this.field('keywords', {boost: 5});
+    this.field('description');
+    this.ref('id');
+
+    /**
+     * Lunr returns a doc { ref: id, score: relevance } after search
+     * index plugin to :
+     *     - override the default `add` method to keep a reference to each full document in the index in a
+     *       new `database` attribute
+     *     - override the default search method to returns the full document with the score instead of the lunr
+     *       default doc
+     */
+    this.use(function(idx) {
+      idx.database = {};  // Map to find a document by its id
+
+      // Override default add method to keep reference to all the documents in the index by their id.
+      var refOriginalAdd = idx.add;
+      idx.add = function(doc, emitEvent) {
+        idx.database[doc[idx._ref]] = doc;
+        refOriginalAdd.apply(idx, [doc, emitEvent]);
+      };
+
+      // Override default search method to returns the real document instead of the lunr result doc
+      var refOriginalSearch = idx.search;
+      idx.search = function (query) {
+        return refOriginalSearch.call(idx, query).map(function(doc) {
+          var article = idx.database[doc.ref];
+          article.score = doc.score;
+          return article;
+        });
+      };
+    });
+  });
+
+  /** Build the lunr index. Called when the data have been downloaded */
+  this._build = function(articles) {
+    var databaseLength = articles.length;
+    var nbIndexed = 0;  // nb of document already indexed
+    var lastProgress = null;  // Last progress value send to the webpage
+
+    articles.forEach(function(article) {
+      this._index.add(article, false);
+      nbIndexed++;
+
+      // Only send indexation progress back to the page if it has changed
+      var progress = Math.floor((nbIndexed / databaseLength) * 100);
+      if (progress !== lastProgress) {
+        this.emitter.sendIndexingEvent(progress);
+        lastProgress = progress;
+      }
+    }.bind(this));
+
+    this.loaded = true;
+    this.emitter.sendReadyEvent();
+  };
+
+  /** Trigger a search query and emit the results */
+  this.search = function(term) {
+    if (!this.loaded) this.emitter.sendErrorEvent('Searchengine not started yet');
+    this.emitter.sendResultEvent(this._index.search(term), term);
+  };
+
+  /** Starts the search engine (download data, build index and switch state to loaded) */
+  this.start = function(dataLoader) {
+    if (this.loaded) this.emitter.sendErrorEvent('Searchengine already started');
+    dataLoader.load(
+      this._build.bind(this),
+      this.emitter.sendErrorEvent,
+      this.emitter.sendDownloadEvent
+    );
+  };
+};
+
+// The search engine instance
+var edSearchEngine = new SearchEngine(logAndPostMessage);
+
+logInfo('started');
+
+// WebWorker message input (coming from webpage)
+onmessage = function(e) {
+  logInfo('onmessage', e.data);
+  switch (e.data.action) {
+    case 'start':
+      logInfo('starting search engine');
+      edSearchEngine.start(new UrlDataLoader(e.data.url));
+      break;
+    case 'search':
+      edSearchEngine.search(e.data.value);
+      break;
+    default:
+      logInfo('unrecognized action ', e.data.action);
+  };
+};
+
+//# sourceMappingURL=worker.js.map
